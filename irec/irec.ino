cb2a2a59ddc4e0a741e61d1afef9bffffb9d3d24
harpo <harpo@harpo.me>
2018-07-10 17:58:31 -0700

    modified dmx to 6 channels, fixed effects maths

--- irec/irec.ino
+++ irec/irec.ino
@@ -2,7 +2,8 @@
 #define STATUS_LED 13
 #define I2C_ADDRESS 42
 
-volatile uint8_t DMX[4];
+#define DMX_CHANNELS 6
+volatile uint8_t DMX[DMX_CHANNELS];
 
 #define USE_HSV
 #include <WS2812.h>
@@ -14,11 +15,27 @@ WS2812 LED(LED_COUNT);
 cRGB black = { 0, 0, 0 };
 cRGB last;
 
-#define DELAY 20
-#define EYESIZE 10
+uint8_t SPEED = 127;
+uint8_t EYESIZE = 10;
 int position = 0;
 
 #include <cos_fix.h>
+
+void positionIncrement() {
+  if (SPEED < 64) {
+    position = (position - (64 - SPEED)) % LED_COUNT;
+  } else if (SPEED < 128) {
+    position = (position - 1 ) % LED_COUNT;
+    delay(SPEED - 64);
+  } else if (SPEED == 128) {
+    //freeze
+  } else if (SPEED < 192) {
+    position = (position + 1 ) % LED_COUNT;
+    delay(192 - SPEED);
+  } else {
+    position = (position + (SPEED - 192)) % LED_COUNT;
+  }
+}
 
 void wash(cRGB color) {
   for (int i = 0; i < LED_COUNT; i++)
@@ -42,53 +59,68 @@ void cylon(cRGB color) {
   LED.set_crgb_at((position - 1) % LED_COUNT, black);
 
   LED.sync();
-  position = (position + 1 ) % LED_COUNT;
+  //position = (position + 1 ) % LED_COUNT;
   //delay(DELAY);
 }
 
 void staticRainbow(cRGB color) {
 
   for (int i = 0; i < LED_COUNT; i++) {
-    LED.set_crgb_at(i, { byte(255 * sin_fix(2 * PI*i / LED_COUNT)),
-                         byte(255 * cos_fix(2 * PI*i / LED_COUNT)),
-                         byte(255 * sin_fix((2 * PI * i + 128) / LED_COUNT))
-                       });
+    LED.set_crgb_at(i, { byte(color.g * (1 + sin(2 * PI*i / LED_COUNT)) / 2),
+                         byte(color.r * (1 + cos(2 * PI*i / LED_COUNT)) / 2),
+                         byte(color.b * (1 + sin((2 * PI * i + 128)) / LED_COUNT)) / 2
+                             });
   }
   LED.sync();
 }
 
 void rainbow(cRGB color) {
   for (int i = 0; i < LED_COUNT; i++) {
-    LED.set_crgb_at(i, {(color.g + position) % 256, (color.r + position) % 256, (color.b - position) % 256 }) ;
+    uint8_t scalar = (1 + sin_fix(2 * PI * i * LED_COUNT)) / 2;
+    LED.set_crgb_at(i, {
+      (color.g + position) % 256,
+      (color.r + position) % 256,
+      (color.b - position) % 256
+    }) ;
   }
   LED.sync();
-  position = (position + 1 ) % LED_COUNT;
+
 }
 
 void rain(cRGB color) {
   for (int i = 0; i < LED_COUNT; i++) {
-    uint8_t scalar = byte(255 * sin(cos(i ^ 2) + sin(i)));
-    LED.set_crgb_at((i + position) % LED_COUNT, {byte(color.g * scalar), byte(color.r * scalar), byte(color.b * scalar)});
+    uint8_t scalar = byte(255 * (1 + sin_fix(i) / 2));
+    LED.set_crgb_at((i + position) % LED_COUNT,
+    {
+      byte(color.g * scalar),
+      byte(color.r * scalar),
+      byte(color.b * scalar)
+    });
   }
   LED.sync();
-  position = (position - 1 ) % LED_COUNT;
+
+  //position = (position - 1 ) % LED_COUNT;
 }
 
 void sparkle(cRGB color) {
 
   for (int i = 0; i < LED_COUNT; i++) {
     uint8_t scalar = byte(255 * sin(cos(i ^ 2) + sin(i)));
-    LED.set_crgb_at(i, {byte(color.g * scalar), byte(color.r * scalar), byte(color.b * scalar)});
+    LED.set_crgb_at(i, {
+      byte(color.g * scalar),
+      byte(color.r * scalar),
+      byte(color.b * scalar)
+    });
   }
   LED.sync();
-  position = (position - 1 ) % LED_COUNT;
+  //position = (position - 1 ) % LED_COUNT;
 }
 
 void newData(int n) {
   for (int i = 0; i < n; i++) {
     DMX[i] = Wire.read();
   }
-  //  Serial.write(DMX, 4);
+  //  Serial.write(DMX, DMX_CHANNELS);
 }
 
 
@@ -106,6 +138,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   analogWrite(STATUS_LED, DMX[0]);
   cRGB c = { DMX[2], DMX[1], DMX[3] };
+  SPEED = DMX[4];
+  EYESIZE = DMX[5];
+
   if (DMX[0] < 25) {
     wash(c);
   } else if (DMX[0] >= 25 && DMX[0] < 51) {
@@ -124,5 +159,6 @@ void loop() {
   } else if (DMX[0] >= 229 && DMX[0] < 255) {
   } // else {error();}
 
+  positionIncrement();
 
 }
--- isend/isend.ino
+++ isend/isend.ino
@@ -4,14 +4,15 @@
 #include <DMXSerial.h>
 #define PACKET_LEN 4
 #define DMX_ADDRESS 100
+#define DMX_CHANNELS 6
 
 #define LED 13
 #define DELAY 0
 const byte I2C_ADDRESS = 42;
 volatile uint8_t VALUE;
 
-uint8_t DMX_PACKET[4] = {0, 1, 2, 3};
-uint8_t DMX_LAST[4];
+uint8_t DMX_PACKET[DMX_CHANNELS] = {0, 1, 2, 3, 4, 5};
+uint8_t DMX_LAST[DMX_CHANNELS];
 void setup() {
   pinModeFast(13, OUTPUT);
   Wire.begin();
@@ -27,7 +28,7 @@ void readDMX() {
 
 void sendPacket() {
   Wire.beginTransmission(I2C_ADDRESS);
-  Wire.write(DMX_PACKET, 4);
+  Wire.write(DMX_PACKET, DMX_CHANNELS);
   while (Wire.endTransmission()) {
   }
 }
@@ -40,9 +41,9 @@ void loop() {
   readDMX();
   analogWrite(LED, DMX_PACKET[0]);
 
-  if (memcmp(DMX_PACKET, DMX_LAST, 4) != 0) {
+  if (memcmp(DMX_PACKET, DMX_LAST, DMX_CHANNELS) != 0) {
     sendPacket();
-    memcpy(DMX_LAST, DMX_PACKET, 4);
+    memcpy(DMX_LAST, DMX_PACKET, DMX_CHANNELS);
   }
 
 }
