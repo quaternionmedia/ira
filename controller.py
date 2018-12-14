5e3be9d6cf349daeb7adbde62958d0905f9abedc
Harpo <harpo@harpo.me>
2018-12-14 12:14:43 -0800

    clearer timer implementation

--- controller.py
+++ controller.py
@@ -1,6 +1,9 @@
 import packet
 from time import sleep
 from datetime import datetime
+
+LIGHTS_ON = 15
+
 commands = [
     [125,50,0,0,190],
     [204,0,255,0,190],
@@ -9,8 +12,14 @@ commands = [
 delay = 15
 print('loading...')
 sleep(2)
-while datetime.now().hour > 15 :
-    for cmd in commands:
-        print('sending {}'.format(cmd))
-        packet.send(*cmd)
-        sleep(delay)
+while True:
+    if datetime.now().hour >= LIGHTS_ON:
+        for cmd in commands:
+            print('sending {}'.format(cmd))
+            packet.send(*cmd)
+            sleep(delay)
+    else:
+        packet.send(0,0,0,0)
+        t = abs(LIGHTS_ON - datetime.now().hour)
+        print('goodnight! See you in {} hours!'.format(t))
+        sleep(t*3600)
