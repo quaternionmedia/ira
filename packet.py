1e84a22e3a5bb6eb89030847ba2e483096ff39fb
Harpo <harpo@harpo.me>
2018-12-17 18:11:50 -0800

    modifed python for multiple COBS messages

--- packet.py
+++ packet.py
@@ -11,14 +11,17 @@ s = Serial(port, BAUD, )
 def send(*args):
 	p = cobs.encode(pack('>{}B'.format(len(args)), *args)) + b'\x00'
 	s.write(p)
-	sleep(.2)
+	sleep(.3)
 	if s.in_waiting:
 		result = s.read(s.in_waiting)
-		if result[-1] == 0:
-			decoded =cobs.decode(result[:-1])
-			values = []
-			for d in decoded:
-				values.append(d)
-			print(values)
+		if result[-1] is 0:
+			results = result.split(b'\x00')
+			results = results[:-1]
+			for r in results:
+				decoded = cobs.decode(r)
+				values = []
+				for d in decoded:
+					values.append(d)
+				print(values)
 		else:
 			print(result)
